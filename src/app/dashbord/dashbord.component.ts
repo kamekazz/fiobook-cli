7a0629423ea9cae7a8ffbc97f7eb258a464ae885
manuel taveras <taveras78@gmail.com>
2018-08-24 23:06:23 -0700

    suscador

--- src/app/dashbord/dashbord.component.ts
+++ src/app/dashbord/dashbord.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { RestApiService } from '../rest-api.service';
 
 @Component({
   selector: 'app-dashbord',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashbord.component.scss']
 })
 export class DashbordComponent implements OnInit {
+  search:any
+  resultOfSerch:any
+  btnDisabled = false
 
-  constructor() { }
+  constructor(
+    private data:DataService,
+    private rest:RestApiService,
+  ) { }
 
   ngOnInit() {
   }
 
+  async onserch(){
+    this.btnDisabled=true
+    try {
+      const data = await this.rest.get(
+        `http://127.0.0.1:3030/api/extre/?search=${this.search}`
+      )
+      if (data['success']) {
+        this.resultOfSerch = data['data']
+        this.data.success(data['message'])
+      } else {
+        this.data.error(data['message'])
+      }
+    } catch (error) {
+      this.data.error(error['message'])
+    }
+  }
 }
